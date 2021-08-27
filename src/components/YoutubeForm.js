33a0b2fe12356b1a39e7257168c8a2aec26d1382
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-08-27 14:55:37 -0300

    validate

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -7,12 +7,32 @@ function YoutubeForm() {
             name: '',
             email: '',
             channel: ''
+        },
+        onSubmit: values => {
+            console.log('Form data', values)
+        },
+        validate: values => {
+            let errors = {}
+
+            if (!values.name) {
+                errors.name = "Required"
+            }
+
+            if (!values.email) {
+                errors.email = "Required"
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Inavlid email format'
+            }
+
+            if (!values.channel) {
+                errors.channel = "Required"
+            }
         }
     })
-    console.log('Formik values:', formik.values)
+    // console.log('Formik values:', formik.values)
     return (
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor='name'>Name</label>
                 <input type='text'
                     id='name' name='name'
@@ -31,9 +51,9 @@ function YoutubeForm() {
                     onChange={formik.handleChange}
                     value={formik.values.channel} />
 
-                <button>Submit</button>
-            </form>
-        </div>
+                <button type="submit">Submit</button>
+            </form >
+        </div >
     )
 }
 
